68088d1dd06f7b84cef500f69e7d9a090bfd9538
Augusto Scher <augustoscher@gmail.com>
2019-05-10 00:18:01 -0300

    refatorando controller

--- app/controllers/admins_backoffice/admins_controller.rb
+++ app/controllers/admins_backoffice/admins_controller.rb
@@ -1,15 +1,15 @@
 class AdminsBackoffice::AdminsController < AdminsBackofficeController
+  before_action :set_admin, only: [:edit,:update]
+
   def index
     @admins = Admin.all
   end
 
+  
   def edit
-    @admin = Admin.find(params[:id])
   end
-
+  
   def update
-    @admin = Admin.find(params[:id])
-    params_admin = params.require(:admin).permit(:email, :password, :password_confirmation)
     if @admin.update(params_admin)
       redirect_to admins_backoffice_admins_path, notice: "Adm saved sucessfuly"
     elsif
@@ -18,4 +18,14 @@ class AdminsBackoffice::AdminsController < AdminsBackofficeController
     end
   end
 
+  private
+  
+  def set_admin
+    @admin = Admin.find(params[:id])
+  end
+
+  def params_admin
+    params_admin = params.require(:admin).permit(:email, :password, :password_confirmation)
+  end
+  
 end
