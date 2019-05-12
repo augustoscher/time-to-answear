83bb7937042bad1a7386733594ee196a956ea086
Augusto Scher <augustoscher@gmail.com>
2019-05-12 19:14:08 -0300

    174 - i18n

--- app/helpers/admins_backoffice_helper.rb
+++ app/helpers/admins_backoffice_helper.rb
@@ -1,2 +1,7 @@
 module AdminsBackofficeHelper
+    def translate_attributes(object = nil, method = nil)
+        if object && method
+            object.model.human_attribute_name(method) 
+        end
+    end
 end
--- config/initializers/locale.rb
+++ config/initializers/locale.rb
@@ -2,4 +2,4 @@
 I18n.available_locales = [:en, "pt-BR"]
 
 # Set the default locale for the app, something other than :en
-I18n.default_locale = 'pt-BR'
+I18n.default_locale = :en
