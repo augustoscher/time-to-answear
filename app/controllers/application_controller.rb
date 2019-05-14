8e7153cfc93895cb4b881493bfae49c42b516893
Augusto Scher <augustoscher@gmail.com>
2019-05-14 12:54:49 -0300

    adicionando layout para devise do admin

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,16 @@
 class ApplicationController < ActionController::Base
+  layout :layout_by_resource
+
+  protected
+
+  def layout_by_resource
+      puts "===== #{devise_controller?}"
+      puts "===== #{resource_class}"
+      if devise_controller? && resource_class == Admin
+          "admin_devise"
+      else 
+          "application"
+      end
+  end
+
 end
