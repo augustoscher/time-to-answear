b86d9f88db46be7a30865686eb29098934d78d9f
Augusto Scher <augustoscher@gmail.com>
2019-05-15 22:06:46 -0300

    ajustando layout do site

---
+++ app/assets/javascripts/user_devise.coffee
@@ -0,0 +1,2 @@
+//= require jquery-2.2.3/dist/jquery
+//= require bootstrap-growl-ifightcrime/jquery.bootstrap-growl.js
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,16 +4,7 @@ class ApplicationController < ActionController::Base
   protected
 
   def layout_by_resource
-
     devise_controller? ? "#{resource_class.to_s.downcase}_devise" : "application"
-
-    # if devise_controller? && resource_class == Admin
-    #   "admin_devise"
-    # elsif devise_controller? && resource_class == User
-    #   "user_devise"
-    # else 
-    #   "application"
-    # end
   end
 
 end
