d121f511f682cc5fe8e1a7f0e246e02a877729c0
Augusto Scher <augustoscher@gmail.com>
2019-05-20 01:50:31 -0300

    adding search on navbar

---
+++ app/controllers/site/search_controller.rb
@@ -0,0 +1,7 @@
+class Site::SearchController < SiteController
+  def questions
+    @questions = Question.includes(:answers)
+      .where("lower(description) LIKE ?", "%#{params[:term].downcase}%")
+      .page(params[:page])
+  end
+end
