de591348aaa2a54ce8c48262394a5c1db72abcbc
Augusto Scher <augustoscher@gmail.com>
2019-05-20 02:13:04 -0300

    realizando pesquisa paginada das questoes

--- app/controllers/site/search_controller.rb
+++ app/controllers/site/search_controller.rb
@@ -1,11 +1,5 @@
 class Site::SearchController < SiteController
   def questions
-    #usar full text search. 
-    #like passa por cada registro da tabela, 
-    #degradando a performance para tabelas onde existem muitos registros.
-    #isso pode ser resolvido com um full text search.
-    @questions = Question.includes(:answers)
-      .where("lower(description) LIKE ?", "%#{params[:term].downcase}%")
-      .page(params[:page])
+    @questions = Question.search(params[:page], params[:term])
   end
 end
--- app/models/question.rb
+++ app/models/question.rb
@@ -5,4 +5,20 @@ class Question < ApplicationRecord
 
   #kaminari
   paginates_per 5
+
+  #usar full text search. 
+  #like passa por cada registro da tabela, 
+  #degradando a performance para tabelas onde existem muitos registros.
+  #isso pode ser resolvido com um full text search.
+  def self.search(page, term)
+    Question.includes(:answers)
+      .where("lower(description) LIKE ?", "%#{term.downcase}%")
+      .page(page)
+  end
+
+  def self.last_questions(page)
+    Question.includes(:answers)
+      .order('created_at desc')
+      .page(params[:page])
+  end
 end
