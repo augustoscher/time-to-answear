cf640683e12ecec18ff9e107721012c98606b79a
Augusto Scher <augustoscher@gmail.com>
2019-05-20 01:54:22 -0300

    explicaçao sobre full text search

--- app/controllers/site/search_controller.rb
+++ app/controllers/site/search_controller.rb
@@ -1,5 +1,9 @@
 class Site::SearchController < SiteController
   def questions
+    #usar full text search. 
+    #like passa por cada registro da tabela, 
+    #degradando a performance para tabelas onde existem muitos registros.
+    #isso pode ser resolvido com um full text search.
     @questions = Question.includes(:answers)
       .where("lower(description) LIKE ?", "%#{params[:term].downcase}%")
       .page(params[:page])
